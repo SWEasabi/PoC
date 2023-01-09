cf6a2823f56fcf61fa1f27c4a1260a6ed7fac070
casYy <51257666+casYy@users.noreply.github.com>
2023-01-09 14:02:59 +0100

    aggiunto metodo put

--- Back-End/Web Server/mqtt-demo/src/main/java/com/SWEasabi/mqttdemo/MqttController.java
+++ Back-End/Web Server/mqtt-demo/src/main/java/com/SWEasabi/mqttdemo/MqttController.java
@@ -3,11 +3,16 @@ package com.SWEasabi.mqttdemo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @RestController
 public class MqttController {
@@ -38,6 +43,39 @@ public class MqttController {
 	    return repository.findById(id)
 	      .orElseThrow(() -> new LampNotFoundException(id));
 	  }
+	  
+	  @PutMapping("/lamps/{id}")
+	  ResponseEntity<?> update(@PathVariable Long id, @RequestBody String message)
+	  {
+		  try {
+
+			  Lamp temp = repository.findById(id).get();
+			  JsonObject rq = new Gson().fromJson(message, JsonObject.class);
+			  
+			  String newStatus = rq.get("status").getAsString();
+			  
+			  System.out.println("Lamp status: " + temp.getStatus());
+			  System.out.println("Message status: " + newStatus);
+			  
+			  if(!temp.getStatus().equals(newStatus))
+			  {
+				  temp.setStatus(newStatus);
+				  repository.save(temp);
+				  mqttGateway.sendToMqtt(newStatus, "lamps/" + id + "/stato");
+				  return ResponseEntity.ok("Eseguito l'update");
+			  }
+			  else
+			  {
+				  return ResponseEntity.ok("La lampadina è già in questo stato");
+			  }
+		  }
+		  catch(Exception ex)
+		  {
+			  return ResponseEntity.ok("Fallito l'update");
+		  }
+	  }
+	  
+	  
 	  
 	  @PostMapping("/addLamps")
 	  Lamp newLamp(@RequestBody Lamp newEmployee) {
