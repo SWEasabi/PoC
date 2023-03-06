590ba7a1a8a78fcd5862386325d5c10d0e2ee3d0
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-03-06 16:56:54 +0100

    Versione Poc con visualizzazione informazioni
    Versione del Poc , con cambiamento icona in base allo stato , viene ora mostrato a display in forma testuale lo stato della lampada , refractoring generale lato angular

--- Front-End/http/src/app/lamp-button/lamp-button.component.ts
+++ Front-End/http/src/app/lamp-button/lamp-button.component.ts
@@ -1,23 +1,41 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { AppService } from '../app.service';
+import { BehaviorSubject, first, tap, delay } from 'rxjs'
+import { StatusData } from '../model';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-lamp-button',
   templateUrl: './lamp-button.component.html',
-  styleUrls: ['./lamp-button.component.css']
+  styleUrls: ['./lamp-button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class LampButtonComponent {
-  constructor(private http: HttpClient) {}
-  
+export class LampButtonComponent implements OnInit {
+  lampStatus$ = new BehaviorSubject<StatusData | null>(null);
 
-  toggleLamp(){
-    
-    this.http.get<any>('http://localhost:8080/lamps/1',{responseType: 'json'}).subscribe(Response => {
-      this.http.put<any>('http://localhost:8080/lamps/1',{responseType: 'json', status: Response.status == 'on' ? 'off' : 'on'})
-      .subscribe(Response => {
-        console.log(Response);
+  constructor(private service: AppService) { }
+
+  ngOnInit(): void {
+    this.getStatus();
+  }
+
+
+  toggleLamp() {
+    this.lampStatus$.pipe(//switchMap operator
+      first(),
+      tap(() => {
+        this.lampStatus$.next(null)
+      }),
+      switchMap((responseData) => this.service.toggleLamp$(responseData?.status))
+    ).subscribe(() => this.getStatus())
+
+
+  }
+  getStatus() {
+    this.service.getData$().pipe(
+      delay(500)).subscribe((statusData) => {
+        this.lampStatus$.next(statusData);
       })
-    })   
   }
 }
